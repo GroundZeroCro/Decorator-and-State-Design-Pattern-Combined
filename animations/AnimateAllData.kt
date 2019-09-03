5047386cd258e047c6d555e2321d6846fe32057d
mario dujic <mario.dujic@gmail.com>
2019-09-03 17:44:07 +0200

    removed base decorator

--- animations/AnimateAllData.kt
+++ animations/AnimateAllData.kt
@@ -1,10 +1,9 @@
 package com.groundzero.sport_now.ui.state.animations
 
 import android.view.View
-import com.groundzero.sport_now.ui.state.ShopStateDecorator
 import com.groundzero.sport_now.ui.state.State
 
-class AnimateAllData(private val visibilityState: State) : ShopStateDecorator(visibilityState) {
+class AnimateAllData(private val visibilityState: State) : State {
 
     override fun changeState(itemSizeParent: View, recyclerParent: View) {
         // Animate and change visibility
